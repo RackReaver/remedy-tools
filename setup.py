205f43c953c8d8ebcab9132062d94f8a2f83c0c2
RackReaver <rackreaver@gmail.com>
2019-02-07 10:20:10 -0500

    small changes

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
-import pathlib
 from setuptools import setup
 
-HERE = pathlib.Path(__file__).parent
-
-README = (HERE / "README.md").read_text()
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    README = f.read()
 
 setup(
     name='remedy-tools',
     version='0.0.1',
     url='https://github.com/RackReaver/remedy-tools',
     license='Apache License',
-    author='RackReaver',
+    author='Matt Ferreira',
     author_email='rackreaver@gmail.com',
     description='Leverages selenium to create tickets in the back-end gui of BMC Remedy.',
     long_description=README,
     long_description_content_type='text/markdown',
     classifiers=[
-        'Development Status :: Pre-Alpha',
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'Intended Audience :: Information Technology',
@@ -24,8 +25,7 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: System :: Systems Administration'
+        'Programming Language :: Python :: 3.7'
     ],
     packages=['remedy_tools'],
     include_package_data=True,
